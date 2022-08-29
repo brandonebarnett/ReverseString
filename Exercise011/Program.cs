7700f37580aa00196500e0a3f886b6eca8d22650
Brandon <brandon.ebarnett@gmail.com>
2022-08-29 16:50:03 -0400

    Add project files.

---
+++ Exercise011/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Write a program that ask's the user to enter their name.  Use an array to reverse
+ * the name and then store the result in a new string.  Display the reversed name on the console.*/
+
+using System.Text;
+
+string userInput;
+
+Console.WriteLine("Please enter your name.");
+userInput = Console.ReadLine();
+
+ReverseName(userInput);
+
+//Console.WriteLine(ReverseName(userInput));
+
+static void ReverseName(string name)
+{
+    char[] chars = name.ToCharArray();
+    StringBuilder reversed = new StringBuilder();
+
+    for (int i = chars.Length - 1; i >= 0; i--)
+    {
+        reversed.Append(chars[i]);
+    }
+    Console.WriteLine(reversed);
+}
